9b30661715408ca6b73730f405f28befe3eead11
Malaga-Karl <kbkmalaga2021@plm.edu.ph>
2024-11-26 23:59:19 +0800

    upload sa uploads foldder

---
+++ src/upload.js
@@ -0,0 +1,22 @@
+document.getElementById('uploadForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch('http://127.0.0.1:5000/upload', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+});
